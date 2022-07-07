2d273191a0517ebd500479d4449108a2401950be
Shreyaar12 <shreyarajpal6@gmail.com>
2022-07-07 15:35:35 +0530

    login added

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import PostsList from './PostsList';
-import App from './App';
-
-export { PostsList, App };
+// import App from './App';
+import Navbar from './Navbar';
+import Home from './Home';
+import Page404 from './Page404';
+import Login from './Login';
+export { PostsList, Navbar, Home, Login, Page404 };
--- src/index.js
+++ src/index.js
@@ -1,36 +1,53 @@
+// // import React from 'react';
+// // import ReactDOM from 'react-dom/client';
+// // import './styles/index.css';
+// // import { App } from './components';
 // import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './styles/index.css';
-// import { App } from './components';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import './index.css';
+// import App from './components/App';
+// import { configureStore } from './store';
+// //  import reportWebVitals from './reportWebVitals';
+
+// // ReactDOM.render(
+// //   <React.StrictMode>
+// //     <App />
+// //   </React.StrictMode>,
+// //   document.getElementById('root')
+// // );
+
+// // // If you want to start measuring performance in your app, pass a function
+// // // to log results (for example: reportWebVitals(console.log))
+// // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// // reportWebVitals();
+
+
+// // ReactDOM.render(
+// //   <React.StrictMode>
+// //     <App />
+// //   </React.StrictMode>,
+// //   document.getElementById('root')
+// // );
+// // reportWebVitals();
+
+
+// const store = configureStore();
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   </Provider>,
+//   document.getElementById('root')
+// );
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
 import { configureStore } from './store';
-//  import reportWebVitals from './reportWebVitals';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-// reportWebVitals();
-
 
 const store = configureStore();
 
